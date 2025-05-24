15eadefffee9ccdc25649bb1900a4ab67834f4a8
RRTDRadin <omega.accessoriesbd@gmail.com>
2025-05-24 10:23:39 -0600

    Add files via upload

---
+++ Python/CW/Operator and data type/home3.py
@@ -0,0 +1,17 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+ex = "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String Multiplied 5 times gives this result :", ex)
+
+word = 'Coding'
+print("Lenght of String :", len(word))
+
+
+
+
+
